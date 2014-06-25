362362153fbdfe8f979ba020bd336d0e58784252
Apostolos Siokas <apostolossiokas@gmail.com>
2014-06-25 10:46:24 +0300

    Add Screenshots
    Add 3 screenshots

---
+++ app/build/generated/source/buildConfig/debug/gr/siokas/plain/app/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package gr.siokas.plain.app;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String PACKAGE_NAME = "gr.siokas.plain.app";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
